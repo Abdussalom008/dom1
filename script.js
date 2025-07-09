2953fe13e5d4f18d700fcb40d1cda7d636b9a903
Abdussalom008 <abdussalomoripov43@gmail.com>
2025-07-09 15:48:05 +0500

    second load

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const lights = document.querySelectorAll('.light');
+const lights = document.querySelectorAll(".light");
 let current = 0;
 
 function changeLight() {
-  lights.forEach(light => light.classList.remove('active'));
-  lights[current].classList.add('active');
-  current = (current + 1) % lights.length;
+  lights.forEach((light) => light.classList.remove("active"));
+  lights[current].classList.add("active");
+  current = current + 1;
+  if (current >= lights.length) {
+    current = 0;
+  }
 }
 
 changeLight();
