bde5a94f08650d69b41a764020d042a856dc4571
Abdussalom008 <abdussalomoripov43@gmail.com>
2025-07-09 15:58:59 +0500

    second load

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let current = 0;
 function changeLight() {
   lights.forEach((light) => light.classList.remove("active"));
   lights[current].classList.add("active");
+
   current = current + 1;
   if (current >= lights.length) {
     current = 0;
